922f1dcc50547a2f11872128568a7bb78550d33b
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-12-30 09:57:42 -0800

    Task 7

--- models/user.py
+++ models/user.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column
 
 
-iclass User(BaseModel, Base):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
 
