391442f6e2d8ad1a7c15bedcd980499e9206d27f
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-01-11 10:46:30 -0800

    Task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,11 +5,14 @@
 from fabric.api import local
 from datetime import datetime as t
 
+
 def do_pack():
     """
         Function: do_pack
         use: generates a .tgz archive from the contents of the web_static
     """
     time = t.now().strftime("%Y%m%d%H%M%S")
-    cmd = "tar -cvzf ./versions/web_static_{}.tgz web_static".format(time)
+    creat = "mkdir -p ./versions/ && "
+    tar_cmd = "tar -cvzf versions/web_static_{}.tgz web_static".format(time)
+    cmd = creat + tar_cmd
     local(cmd)
