06df39e9f8cd8c9de356104d3c45427f794c9ae8
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-01-11 06:33:32 -0800

    Task 0 :)

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,13 +4,7 @@ sudo apt-get update -y
 sudo apt-get install -y nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo '<html>
-<head>
-</head>
-<body>
-Holberton School
-</body>
-</html>' > /data/web_static/releases/test/index.html
+echo 'Hi Dear' > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -hR ubuntu:ubuntu /data/
 sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
