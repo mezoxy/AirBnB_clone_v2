1247ed9bfc061607ce8c4e3bc2b2ae12f2db5d43
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-01-11 07:16:11 -0800

    Task 0 :)

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # Set up your web servers for the deployment of web_static
-apt-get update -y
-apt-get install -y nginx
-service nginx start
+sudo apt-get update -y
+sudo apt-get install -y nginx
+sudo service nginx start
 mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
-chown -hR ubuntu:ubuntu /data/
-echo "<h1>Hi Dear, I'm Ayoub</h1>" > /data/web_static/releases/test/index.html
+echo 'Hi Dear, Welcom To our Site' > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-sed -i "s#server_name _;#server_name _;\n\n\tlocation /hbnb_static {\n\t\talias  /data/web_static/current/;\n\t}#" /etc/nginx/sites-enabled/default
-sudo service nginx restart
+chown -hR ubuntu:ubuntu /data/
+sudo sed -i "s#server_name _;#server_name _;\n\n\tlocation /hbnb_static {\n\t\talias  /data/web_static/current/;\n\t}#" /etc/nginx/sites-enabled/default
+service nginx restart
