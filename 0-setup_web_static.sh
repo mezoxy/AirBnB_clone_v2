ec4413d2bafdf34e9544cb5e691c5c0304d4f0e9
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-01-11 07:20:21 -0800

    Task 0 :)

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,9 +3,9 @@
 sudo apt-get update -y
 sudo apt-get install -y nginx
 sudo service nginx start
-mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 echo 'Hi Dear, Welcom To our Site' > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -hR ubuntu:ubuntu /data/
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i "s#server_name _;#server_name _;\n\n\tlocation /hbnb_static {\n\t\talias  /data/web_static/current/;\n\t}#" /etc/nginx/sites-enabled/default
 service nginx restart
