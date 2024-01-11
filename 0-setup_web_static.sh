2148e3e20d17075ea8f19041477c52754225a4fc
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-01-11 06:54:40 -0800

    Task 0 :)

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,6 +6,6 @@ sudo service nginx start
 sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 sudo chown -Rh ubuntu:ubuntu /data/
 echo "<h1>Hi Dear, I'm Ayoub</h1>" > /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo sed -i "s#server_name _;#server_name _;\n\n\tlocation /hbnb_static {\n\t\talias  /data/web_static/current/;\n\t}#" /etc/nginx/sites-enabled/default
 sudo service nginx restart
