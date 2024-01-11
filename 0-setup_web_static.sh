5670acf445d8f302984ea81ac7aabf7d7986475c
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-01-11 06:24:51 -0800

    Task 0 :)

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,10 +2,32 @@
 # Set up your web servers for the deployment of web_static
 sudo apt-get update -y
 sudo apt-get install -y nginx
-sudo service nginx start
-sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
-sudo chown -Rh ubuntu:ubuntu /data/
-echo "<h1>Hi Dear, I'm Ayoub</h1>" > /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo sed -i "s#server_name _;#server_name _;\n\n\tlocation /hbnb_static {\n\t\talias  /data/web_static/current/;\n\t}#" /etc/nginx/sites-enabled/default
-sudo service nginx restart
+mkdir -p /data/web_static/releases/test/
+# Create the folder /data/ if it doesn’t already exist
+# Create the folder /data/web_static/ if it doesn’t already exist
+# Create the folder /data/web_static/releases/ if it doesn’t already exist
+# Create the folder /data/web_static/releases/test/ if it doesn’t already exist
+mkdir -p /data/web_static/shared/
+# Create the folder /data/web_static/shared/ if it doesn’t already exist
+echo '<html>
+<head>
+</head>
+<body>
+Holberton School
+</body>
+</html>' > /data/web_static/releases/test/index.html
+# Create a fake HTML file /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Create a symbolic link /data/web_static/current linked to the...
+# .../data/web_static/releases/test/ folder. If the symbolic link already...
+# ...exists, it should be deleted and recreated every time the script is ran.
+chown -hR ubuntu:ubuntu /data/
+# Give ownership of the /data/ folder to the ubuntu user AND group (you can
+# assume this user and group exist). This should be recursive; everything
+# inside should be created/owned by this user/group.
+sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+# Update the Nginx configuration to serve the content of
+# /data/web_static/current/ to hbnb_static
+# (ex: https://mydomainname.tech/hbnb_static).
+# Don’t forget to restart Nginx after updating the configuration:
+service nginx restart
