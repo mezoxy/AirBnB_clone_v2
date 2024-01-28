bbea6ec524527f7aa48a661fdaaffdfe78555a7f
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-01-28 12:00:09 -0800

    Task 7

--- models/state.py
+++ models/state.py
@@ -11,13 +11,16 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='all, delete-orphan')
+    cities = relationship(
+            'City', backref='state', cascade='all, delete-orphan')
 
     @property
     def cities(self):
-        """Getter attribute cities that returns the list of City instances 
+        """Getter attribute cities that returns
+            the list of City instances
             with state_id equals to the current State.id
         """
         from models import storage
         if getenv('HBNB_TYPE_STORAGE') != 'db':
-            return [obj for obj in storage.all(City) if self.id == obj.state_id]
+            return [obj for obj in storage.all(
+                City) if self.id == obj.state_id]
