40d767a08f6427abc65f084d35ae0dad37888d74
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-01-28 12:04:10 -0800

    Task 7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -69,10 +69,10 @@ class DBStorage:
         session_factory = sessionmaker(
                 bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
-        self.__session = Session
+        self.__session = Session()
 
     def close(self):
         """
             Close: Close the local session
         """
-        self.__session.remove()
+        self.__session.close()
